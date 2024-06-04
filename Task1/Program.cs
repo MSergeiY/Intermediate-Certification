8920990e2bbb74335e718bc941421cd691ac6518
SergeyM <rus.med.290500@gmail.com>
2024-06-04 12:50:32 +0600

    Added Task1 Solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,7 +2,28 @@
 // Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}");
+        return;
+    }
+    else if (m > n)
+    {
+        Console.Write($"{m} ");
+        PrintNaturalNumbers(m - 1, n);
+    }
+    else if (m < n)
+    {
+        Console.Write($"{n} ");
+        PrintNaturalNumbers(m, n - 1);
+    }
+
+}
+
 Console.WriteLine("Введите начало промежутка(M): ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите конец промежутка(N): ");
 int n = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNumbers(m, n);
--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ca24a6a3a31d34ea68511f7d269873d50cc165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0932304cfe7a35dd2474c05c88898a45e4841738")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
