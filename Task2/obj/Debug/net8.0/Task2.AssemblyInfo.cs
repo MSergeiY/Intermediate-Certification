57cdcc172449c97d217dcab28cd8419afb8f3b0a
SergeyM <rus.med.290500@gmail.com>
2024-06-04 13:02:39 +0600

    Create Random Array Task 3

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ca24a6a3a31d34ea68511f7d269873d50cc165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6bc881e60fa800ea34ec4fe91e8d13dfd14bed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задайте произвольный массив. 
 // Выведете его элементы, начиная с конца. 
 // Использовать рекурсию, не использовать циклы.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
